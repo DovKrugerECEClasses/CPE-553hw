64e86044cc9c5c20e6d1137a6170bcb510ff6cb0
Dov Kruger <Dov.Kruger@gmail.com>
2018-11-13 14:26:09 -0500

    Added example Exception class

---
+++ buildlibrary/src/Ex.hh
@@ -0,0 +1,14 @@
+class Ex {
+private:
+	static const char** messages;
+	static void loadErrors(const char filename[]);
+public:
+	Ex(const char filename[], uint32_t linenum, uint32_t message) : 
+};
+
+enum MyErrors {
+	ERR_CANNOT_OPEN,
+	ERR_PERMISSION_DENIED,
+	
+	
+};
